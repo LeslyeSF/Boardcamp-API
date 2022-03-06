431ba0ae068f966db7534532cf457ac380b65266
Leslye Soares Ferreira <leslyesoares@gmail.com>
2022-03-06 03:14:36 -0300

    feat: rentals routers

--- src/routers/rentalsRouters.js
+++ src/routers/rentalsRouters.js
@@ -8,7 +8,13 @@ const rentalsRouters = express.Router();
 rentalsRouters.get('/rentals', async (req, res) => {
   try {
     const listRentals = await connection.query(
-      'SELECT * FROM rentals JOIN games ON "gameId"=games.id'
+      `SELECT rentals.*, 
+      jsonb_build_object('name', customers.name, 'id', customers.id) AS customer,
+      jsonb_build_object('id', games.id, 'name', games.name, 'categoryId', games."categoryId", 'categoryName', categories.name) AS games
+      FROM rentals 
+      JOIN customers ON rentals."customerId"=customers.id
+      JOIN games ON rentals."gameId"=games.id
+      JOIN categories ON categories.id = games."categoryId"`
     );
     res.status(200).send(listRentals.rows);
   } catch (err) {
@@ -66,11 +72,61 @@ rentalsRouters.post('/rentals', async (req, res) => {
   }
 });
 
-rentalsRouters.post('/rentals/:id/return', (req, res) => {
-  res.send('post/rentals/:id/return');
+rentalsRouters.post('/rentals/:id/return', async (req, res) => {
+  try {
+    const rental = await connection.query('SELECT * FROM rentals WHERE id=$1', [
+      req.params.id,
+    ]);
+
+    if (rental.rowCount === 0) {
+      res.sendStatus(404);
+      return;
+    }
+    if (rental.rows[0].returnDate) {
+      res.sendStatus(400);
+      return;
+    }
+    const game = await connection.query('SELECT * FROM games WHERE id=$1', [
+      rental.rows[0].gameId,
+    ]);
+    const { rentDate } = rental.rows[0];
+    const returnDate = dayjs().format('YYYY-MM-DD');
+    const delayFee =
+      // eslint-disable-next-line radix
+      (parseInt(dayjs().format('D')) -
+        (rentDate.getDate() + rental.rows[0].daysRented)) *
+      game.rows[0].pricePerDay;
+    await connection.query(
+      'UPDATE rentals SET "returnDate"=$1, "delayFee"=$2 WHERE id=$3',
+      [returnDate, delayFee, req.params.id]
+    );
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
-rentalsRouters.delete('/rentals/:id', (req, res) => {
-  res.send('delete/rentals/:id');
+
+rentalsRouters.delete('/rentals/:id', async (req, res) => {
+  try {
+    const rental = await connection.query('SELECT * FROM rentals WHERE id=$1', [
+      req.params.id,
+    ]);
+
+    if (rental.rowCount === 0) {
+      res.sendStatus(404);
+      return;
+    }
+
+    if (rental.rows[0].returnDate) {
+      res.sendStatus(400);
+      return;
+    }
+
+    await connection.query('DELETE FROM rentals WHERE id=$1', [req.params.id]);
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 export default rentalsRouters;
